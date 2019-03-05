859138de2ff9624ffe17e4ca13b21122e76cd45a
Matt McMurry <matt-mcmurry@pluralsight.com>
2019-03-04 23:09:54 -0700

    marzipan research

--- PSOperations/PhotosCapability.swift
+++ PSOperations/PhotosCapability.swift
@@ -1,33 +1,33 @@
 #if os(iOS)
 
 import Foundation
-import Photos
-
-public struct Photos: CapabilityType {
-    public static let name = "Photos"
-
-    public init() { }
-
-    public func requestStatus(_ completion: @escaping (CapabilityStatus) -> Void) {
-        let status = PHPhotoLibrary.authorizationStatus()
-        switch status {
-        case .authorized: completion(.authorized)
-        case .denied: completion(.denied)
-        case .restricted: completion(.notAvailable)
-        case .notDetermined: completion(.notDetermined)
-        }
-    }
-
-    public func authorize(_ completion: @escaping (CapabilityStatus) -> Void) {
-        PHPhotoLibrary.requestAuthorization { status in
-            switch status {
-            case .authorized: completion(.authorized)
-            case .denied: completion(.denied)
-            case .restricted: completion(.notAvailable)
-            case .notDetermined: completion(.notDetermined)
-            }
-        }
-    }
-}
+//import Photos
+//
+//public struct Photos: CapabilityType {
+//    public static let name = "Photos"
+//
+//    public init() { }
+//
+//    public func requestStatus(_ completion: @escaping (CapabilityStatus) -> Void) {
+//        let status = PHPhotoLibrary.authorizationStatus()
+//        switch status {
+//        case .authorized: completion(.authorized)
+//        case .denied: completion(.denied)
+//        case .restricted: completion(.notAvailable)
+//        case .notDetermined: completion(.notDetermined)
+//        }
+//    }
+//
+//    public func authorize(_ completion: @escaping (CapabilityStatus) -> Void) {
+//        PHPhotoLibrary.requestAuthorization { status in
+//            switch status {
+//            case .authorized: completion(.authorized)
+//            case .denied: completion(.denied)
+//            case .restricted: completion(.notAvailable)
+//            case .notDetermined: completion(.notDetermined)
+//            }
+//        }
+//    }
+//}
 
 #endif
